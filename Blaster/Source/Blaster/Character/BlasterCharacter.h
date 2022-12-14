cf04aa88821a924ec7e86c4f9eedb6ab16d60802
moonabyss <d-v@ukr.net>
2022-12-14 20:39:10 +0200

    03.10. Camera and Spring Arm

--- Blaster/Source/Blaster/Character/BlasterCharacter.h
+++ Blaster/Source/Blaster/Character/BlasterCharacter.h
@@ -6,24 +6,32 @@
 #include "GameFramework/Character.h"
 #include "BlasterCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
 UCLASS()
 class BLASTER_API ABlasterCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	ABlasterCharacter();
 
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+	
+protected:
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+private:
+	UPROPERTY(Category="Camera", VisibleAnywhere)
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(Category = "Camera", VisibleAnywhere)
+	UCameraComponent* FollowCamera;
 
 };
